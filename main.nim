c853434139e0c9b7f5649705ee575a9b71f5d260
hlaaftana <atadorukaydin@gmail.com>
2018-03-29 16:32:44 +0300

    Added methods of epiccing

--- main.nim
+++ main.nim
@@ -1,9 +1,20 @@
 import common, json, ws, asyncdispatch, http, uri
 
+proc sendMessage(channelId: string, content: string, tts: bool = false): Future[Response] =
+  var payload = newJObject()
+  payload["content"] = %content
+  if tts: payload["tts"] = %true
+  post(api / "channels" / channelId / "messages", $payload)
+
+proc reply(obj: JsonNode, content: string, tts: bool = false): Future[Response] =
+  sendMessage(obj["channel_id"].getStr, content, tts)
+
+# main bot
+
 proc message(obj: JsonNode) =
   let content = obj["content"].getStr
   if content == "dab now":
-    asyncCheck post(api / "channels" / obj["channel_id"].getStr / "messages", %*{"content": "\\*dabs now\\*"})
+    asyncCheck obj.reply("\\*dabs now\\*")
 
 addListener(messageEvent, message)
 
