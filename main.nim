211b5d848675588c2d249573e40b7f05ade1fd62
hlaaftana <atadorukaydin@gmail.com>
2018-03-30 09:51:35 +0300

    ğğğğğğğğ

--- main.nim
+++ main.nim
@@ -1,12 +1,12 @@
 import common, json, ws, asyncdispatch, http, uri
 
-proc sendMessage(channelId: string, content: string, tts: bool = false): Future[Response] =
+proc sendMessage(channelId: string, content: string, tts: bool = false): auto =
   var payload = newJObject()
   payload["content"] = %content
   if tts: payload["tts"] = %true
-  post(api / "channels" / channelId / "messages", $payload)
+  post(api / "channels" / channelId / "messages", payload)
 
-proc reply(obj: JsonNode, content: string, tts: bool = false): Future[Response] =
+proc reply(obj: JsonNode, content: string, tts: bool = false): auto =
   sendMessage(obj["channel_id"].getStr, content, tts)
 
 # main bot
